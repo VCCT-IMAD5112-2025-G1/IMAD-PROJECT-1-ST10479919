cab66eb5d229ff4060c378c17f372cca766dab62
Jaco <ST10479919@vcconnect.edu.za>
2025-03-24 20:47:31 +0200

    Updated time handling and added menu options

--- app/src/main/java/com/example/chronochef/MainActivity.kt
+++ app/src/main/java/com/example/chronochef/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.chronochef
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
@@ -9,7 +10,6 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import java.sql.Time
 import java.text.SimpleDateFormat
 import java.util.Locale
 
@@ -18,66 +18,53 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.testingView)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
         val userInputTime = findViewById<EditText>(R.id.userInputTime)
-        val testInput = findViewById<TextView>(R.id.testingView)
-
-        val morningStartTime = "6:00"
-        val morningEndTime = "11:59"
-        val afternoonStartTime = "12:00"
-        val afternoonEndTime = "17:59"
-        val eveningStartTime = "18:00"
-        val eveningEndTime = "21:59"
-
-        // Convert string to int and use time as minutes
-        val morningStartTimeMinuets = timeToMinutes(morningStartTime)
-        val morningEndTimeMinuets = timeToMinutes(morningEndTime)
-        val afternoonStartTimeMinutes = timeToMinutes(afternoonStartTime)
-        val afternoonEndTimeMinutes = timeToMinutes(afternoonEndTime)
-        val eveningStartTimeMinutes = timeToMinutes(eveningStartTime)
-        val eveningEndTimeMinutes = timeToMinutes(eveningEndTime)
-
-
+        val userReference = findViewById<TextView>(R.id.userReference)
 
         userInputTime.setOnClickListener {
             val userTime = userInputTime.text.toString()
             val userTimeMinutes = timeToMinutes(userTime)
-            val tofoodActivity = Intent(this, foodActivity::class.java)
+
 
             // Checks is input valid
             if (isValidTime(userTime)) {
-                // TODO: Change the testInput to go to own screen for food options
-                // TODO: Make input clear after switching screen
+
                 when (userTimeMinutes) {
-                    in morningStartTimeMinuets..morningEndTimeMinuets -> {
-                        testInput.text = "Morning"
-                        intent.putExtra("Morning",userInputTime.text.toString())
+                    in timeToMinutes("6:00")..timeToMinutes("11:59") -> {
+                        userReference.text = "Morning"
                     }
-                    in afternoonStartTimeMinutes..afternoonEndTimeMinutes -> {
-                        testInput.text = "Afternoon"
-                        intent.putExtra("Afternoon",userInputTime.text.toString())
+                    in timeToMinutes("12:00")..timeToMinutes("15:00") -> {
+                        userReference.text = "Afternoon"
                     }
-                    in eveningStartTimeMinutes..eveningEndTimeMinutes -> {
-                        testInput.text = "Evening"
-                        intent.putExtra("Evening",userInputTime.text.toString())
+                    in timeToMinutes("15:01")..timeToMinutes("15:59") ->{
+                        userReference.text = "Mid-Afternoon"
+                    }
+                    in timeToMinutes("16:00")..timeToMinutes("20:00") -> {
+                        userReference.text = "Evening"
                     }
                     else -> {
-                        testInput.text = "Night"
-                        intent.putExtra("Night",userInputTime.text.toString())
+                        userReference.text = "Night"
                     }
-                }
-                // Moves to foodActivity
-                startActivity(tofoodActivity)
 
+                }
+                val toFoodActivity = Intent(this, foodActivity::class.java)
+                // Takes user input to place in other activity
+                toFoodActivity.putExtra("userInput",userReference.text.toString())
+                // Moves to FoodActivity
+                startActivity(toFoodActivity)
+
+                userInputTime.text.clear()
+                userReference.visibility = View.GONE
             }
             // Notify user of format
             else {
-                Toast.makeText(this, "Please enter a valid time: HH/mm", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "Please enter a valid time: HH/mm" + " 24 hour format", Toast.LENGTH_SHORT).show()
             }
         }
     }
--- app/src/main/java/com/example/chronochef/foodActivity.kt
+++ app/src/main/java/com/example/chronochef/foodActivity.kt
@@ -1,20 +1,50 @@
 package com.example.chronochef
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import org.w3c.dom.Text
 
 class foodActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_food)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.testingView)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        // Get user input from other Activity
+        val bundle: Bundle? = intent.extras
+        val userInput: String? = bundle?.getString("userInput")
+
+        val menuList = findViewById<TextView>(R.id.testKey)
+        val cardHeader = findViewById<TextView>(R.id.cardHeader)
+
+        // Change what happens based on what user input
+        // TODO: Add food for user
+        when (userInput) {
+            "Morning" -> {
+                cardHeader.text = "Breakfast"
+                menuList.text = "Eggs and bacon"
+            }
+            "Afternoon" -> {
+                cardHeader.text = "Lunch"
+                menuList.text = "Tea and Biscuits"
+            }
+            "Evening" -> {
+                cardHeader.text = "Dinner"
+                menuList.text = "Evening"
+            }
+            else -> {
+                cardHeader.text = "Night snack"
+                menuList.text = "Night"
+            }
+        }
+
     }
 }
